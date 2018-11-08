b5f5053f2b7b3f546ffcd53546a4f22f89f40f26
SEIlSm <ilya.smolnikov@ascom.com>
2018-11-08 15:24:38 +0300

    Add setup for unittests and add docstrings

--- Readme.md
+++ Readme.md
@@ -1,6 +1,15 @@
 # Script for check YAML test case files structure
 
 [![Check PEP8](https://travis-ci.org/kodSIM/yamlchecker.svg?branch=master)](https://travis-ci.org/kodSIM/yamlchecker)
+[![PyPI version](https://badge.fury.io/py/yamlchecker.svg)](https://badge.fury.io/py/yamlchecker)
+
+## Installation
+
+For install `yamlchecker` use `pip`:
+
+    pip install yamlchecker
+
+## Running
 
 For run script use format:
 
--- tests/test_yamlchecker.py
+++ tests/test_yamlchecker.py
@@ -6,16 +6,20 @@ import pytest
 import yamlchecker
 
 
-TRUTH_YAML = """
-Description: description.
+TRUTH_FILE = 'truth.yaml'
+FAIL_FILE = 'fail.yml'
+FAIL_SECTION_FILE = 'fail_section.yaml'
+DIRECTORY = 'test_cases'
+
+TRUTH_YAML = """Description: description.
 Requirements: req_id
 Steps:
     -
         Description: step 1.
         Expected result: result for step 1.
 """
-FAIL_YAML = """
-Description: |
+
+FAIL_YAML = """Description: |
 -
     description.
 Requirements: req_id
@@ -24,55 +28,85 @@ Steps:
         Description: step 1.
         Expected result: result for step 1.
 """
-FAIL_SECTION = """
-Descruption: |
+
+FAIL_SECTION = """Descruption: |
     description.
 Steps:
     -
         Description: step 1.
         Expected result: result for step 1.
 """
+
 SECTION = {
     'Description': 'description'
 }
 
 
-def create_truth_file(tmpdir):
-    file = tmpdir.join('file.yaml')
-    file.write(TRUTH_YAML)
-    return file
+@pytest.fixture(autouse=True)
+def setup(tmpdir):
+    """Setup for unit tests.
 
-
-def create_fail_file(tmpdir):
-    file = tmpdir.join('file.yaml')
-    file.write(FAIL_YAML)
-    return file
-
-
-def create_file_with_fail_sections(tmpdir):
-    file = tmpdir.join('file.yaml')
-    file.write(FAIL_SECTION)
-    return file
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
+    truth_file = tmpdir.join(TRUTH_FILE)
+    truth_file.write(TRUTH_YAML)
+    fail_file = tmpdir.join(FAIL_FILE)
+    fail_file.write(FAIL_YAML)
+    fail_section_file = tmpdir.join(FAIL_SECTION_FILE)
+    fail_section_file.write(FAIL_SECTION)
+    subdir = tmpdir.mkdir(DIRECTORY)
+    subdir.join('file1.yaml').write(TRUTH_YAML)
+    subdir.join('file2.yml').write(FAIL_YAML)
+    subdir.join('file3.txt').write(FAIL_YAML)
 
 
 def test_truth_file(tmpdir):
-    file = create_truth_file(tmpdir)
-    assert yamlchecker.yaml_checker(Path(str(file))) == 0
+    """Test for truth file.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
+    file = str(tmpdir.join(TRUTH_FILE))
+    assert yamlchecker.yaml_checker(Path(file)) == 0
 
 
 def test_fail_file(tmpdir):
-    file = create_fail_file(tmpdir)
-    assert yamlchecker.yaml_checker(Path(str(file))) == 2
+    """Test for fail file.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
+    file = str(tmpdir.join(FAIL_FILE))
+    assert yamlchecker.yaml_checker(Path(file)) == 2
 
 
 def test_file_with_fail_sections(tmpdir):
-    file = create_file_with_fail_sections(tmpdir)
-    assert yamlchecker.yaml_checker(Path(str(file))) == 2
+    """Test for file with fail sections.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
+    file = str(tmpdir.join(FAIL_SECTION_FILE))
+    assert yamlchecker.yaml_checker(Path(file)) == 2
+
+
+def test_directory_with_tests(tmpdir):
+    """Test for directory with test cases.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
+    directory = str(tmpdir.join(DIRECTORY))
+    assert yamlchecker.yaml_checker(Path(directory)) == 2
 
 
 def test_truth_section(tmpdir):
+    """Test for truth section.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
     assert yamlchecker.yamlchecker.check_section(SECTION, 'Description', tmpdir) == 0
 
 
 def test_fail_section(tmpdir):
+    """Test for fail section.
+
+    :param py.local.path tmpdir: Fixture for path to temporary directory.
+    """
     assert yamlchecker.yamlchecker.check_section(SECTION, 'Steps', tmpdir) == 1
